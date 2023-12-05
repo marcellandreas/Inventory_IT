202691bc5c7ec651ec08773ae1043a21bea9d0c1
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2023-12-05 15:12:30 +0700

    success merge

--- fe-inventory-it-bantex/src/example.test.js
+++ fe-inventory-it-bantex/src/example.test.js
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // untuk menggunakan ekstensi expect dari testing-library
 import { MainLayout } from "./components/templates";
 
-test("renders component with correct text", () => {
+test("renders component with correct text ", () => {
   render(<MainLayout />);
   expect(screen.getByText("Hello, World!")).toBeInTheDocument();
 });
